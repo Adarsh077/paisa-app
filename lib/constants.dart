0f241700de7ad75fdcb08545a240fccac4cd6743
Adarsh Senghani <adarshco077@gmail.com>
2025-05-11 22:42:22 +0530

    fix: updated api endpoint for agent

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class AppConstants {
   static final String baseUrl =
-      kReleaseMode ? 'https://api.production.com' : 'http://192.168.0.104:8001';
+      kReleaseMode ? 'http://13.203.202.169:8001' : 'http://192.168.0.104:8001';
   static final String agentBaseUrl =
-      kReleaseMode ? 'https://api.production.com' : 'http://192.168.0.104:8002';
+      kReleaseMode ? 'http://13.203.202.169:8002' : 'http://192.168.0.104:8002';
 }
